a0ae0ada86bdca9ab87f013a2f0728de65f1dcee
jamie-cloud99 <j1999j88@gmail.com>
2023-06-07 09:31:41 +0800

    重構篩選功能

--- src/components/ApiList.vue
+++ src/components/ApiList.vue
@@ -10,6 +10,7 @@
           id="formId1"
           placeholder="搜尋"
           v-model="searchWords"
+          @input="paginate(searchTools)"
         />
         <label for="formId1" class="text-muted ps-md-4">
           <span class="me-2">
@@ -35,9 +36,19 @@
               <p class="fs-xs fw-bold text-black-60 px-3 my-2">{{ filter.select }}</p>
               <ul class="list-group">
                 <li class="list-item" v-for="item in filter.options" :key="item">
-                  <button type="button" class="dropdown-item" @click="filterTools(item, filter.select)" :class="{active: filtersSelected.some(value => value.filter===item && value.type === filter.select) }">{{ item }}</button>
+                  <button
+                    type="button"
+                    class="dropdown-item"
+                    @click="toggleFilter(item, filter)"
+                    :class="{ active: (filtersSelected.model === item || filtersSelected.type === item) }"
+                  >
+                    {{ item }}
+                  </button>
                 </li>
-                <li v-show="filter.id === 1" class="split-line border-bottom border-black-20 rounded-0"></li>
+                <li
+                  v-show="filter.id === 1"
+                  class="split-line border-bottom border-black-20 rounded-0"
+                ></li>
               </ul>
             </li>
           </ul>
@@ -105,7 +116,7 @@
             {{ item }}
           </button>
         </li>
-    </ul> 
+      </ul>
 
       <div class="card-list overflow-hidden mb-4">
         <ul class="row gy-3">
@@ -138,7 +149,10 @@
           </li>
         </ul>
       </div>
-      <PaginationComponent :pagination="tempPagination" @change_page="changePage"></PaginationComponent>
+      <PaginationComponent
+        :pagination="tempPagination"
+        @change_page="changePage"
+      ></PaginationComponent>
     </div>
   </div>
 </template>
@@ -150,7 +164,7 @@ import ShareIcon from 'vue-material-design-icons/ShareVariant.vue'
 import PaginationComponent from './PaginationComponent.vue'
 
 export default {
-  components:{
+  components: {
     SearchIcon,
     FilterIcon,
     ShareIcon,
@@ -167,26 +181,36 @@ export default {
         {
           id: 2,
           select: '類型',
-          options: ['全部', '問答服務', '虛擬客服', '生活應用', '程式知識', '翻譯助手', '行銷文案']
+          options: [
+            '所有類型',
+            '問答服務',
+            '虛擬客服',
+            '生活應用',
+            '程式知識',
+            '翻譯助手',
+            '行銷文案'
+          ]
         }
       ],
       sort: ['由新到舊', '由舊到新'],
-      filtersSelected: [],
+      filtersSelected: {},
       isFromNew: true,
       tempApplicationsBox: {
-        apps: [{
-          title: ''
-        }]
+        apps: [
+          {
+            title: ''
+          }
+        ]
       },
       applicationsBox: {
         type: ['全部', '問答服務', '虛擬客服', '生活應用', '程式知識', '翻譯助手', '行銷文案'],
         typeSelected: '全部',
-        modelSelected: '全部',
+        modelSelected: '所有模型',
         apps: []
       },
-      pagination: { 
-        current_page: 1, 
-        total_pages: 1, 
+      pagination: {
+        current_page: 1,
+        total_pages: 1,
         has_next: true,
         has_pre: false
       },
@@ -197,101 +221,81 @@ export default {
   methods: {
     sortTools(isFromNew) {
       this.isFromNew = isFromNew
-      isFromNew ? this.tempApplicationsBox.apps.sort((a,b) => b.create_time - a.create_time) : this.tempApplicationsBox.apps.sort((a,b) => a.create_time - b.create_time)
+      isFromNew
+        ? this.tempApplicationsBox.apps.sort((a, b) => b.create_time - a.create_time)
+        : this.tempApplicationsBox.apps.sort((a, b) => a.create_time - b.create_time)
     },
-    filterTools(filter, type) {
-      // 多選filters  toggle
-      const newFilter = { filter, type }
-      if (this.filtersSelected.some(value=> value.type === type && value.filter === filter)) {
-        const index = this.filtersSelected.indexOf(newFilter)
-        this.filtersSelected.splice(index, 1)
-
-
-        if (type === '類型') {
-          this.applicationsBox.typeSelected = '全部'
-        } else {
-          this.applicationsBox.modelSelected = '全部'
+    toggleFilter(item, filter) {
+      if (filter.id === 1) {
+        if (item !== this.applicationsBox.modelSelected) {
+          this.applicationsBox.modelSelected = item
         }
-
-      } else if (this.filtersSelected.some(value=> value.type.includes(type))) {
-        const index = this.filtersSelected.findIndex(value=> value.type.includes(type))
-        this.filtersSelected.splice(index, 1)
-        this.filtersSelected.push(newFilter)
-
-        if(type === '類型'){
-          this.applicationsBox.typeSelected = filter
-        } else {
-          this.applicationsBox.modelSelected = filter
-        }
-
       } else {
-        this.filtersSelected.push(newFilter)
-
-        if(type === '類型'){
-          this.applicationsBox.typeSelected = filter
-        } else {
-          this.applicationsBox.modelSelected = filter
+        if (item === '所有類型') item = '全部'
+        if (item !== this.applicationsBox.typeSelected) {
+          this.applicationsBox.typeSelected = item
         }
       }
-
-      this.applicationsBox.typeSelected = this.applicationsBox.typeSelected.replace('所有類型', '全部')
-      this.applicationsBox.modelSelected = this.applicationsBox.modelSelected.replace('所有模型', '全部')
-
-      if (this.filtersSelected.length === 0) {
-        this.applicationsBox.typeSelected = '全部'
-        this.applicationsBox.modelSelected = '全部'
+      (item === '全部') ? this.filtersSelected.type = "所有類型" : this.filtersSelected.type = this.applicationsBox.typeSelected
+      this.filtersSelected.model = this.applicationsBox.modelSelected
+      
+      this.filterTools(this.filtersSelected)
+    },
+    filterTools(filters){
+      if(filters.model !== "所有模型" && filters.type !== "所有類型") {
+        this.tempApplicationsBox.apps = this.applicationsBox.apps.filter(app => (app.model === filters.model && app.type === filters.type))
+      } else if (filters.model === "所有模型" && filters.type !== "所有類型") {
+        this.tempApplicationsBox.apps = this.applicationsBox.apps.filter(app => app.type === filters.type)
+      } else if (filters.model !== "所有模型" && filters.type === "所有類型") {
+        this.tempApplicationsBox.apps = this.applicationsBox.apps.filter(app => app.model === filters.model)
+      } else {
+        this.tempApplicationsBox.apps = [...this.applicationsBox.apps]
       }
-
-      this.showTool(this.applicationsBox)
+      this.paginate(this.tempApplicationsBox.apps)
     },
-    showTool(applicationsBox){
-      this.tempApplicationsBox.apps = [...applicationsBox.apps]
-      // [], [m, c]
-    },
-    async fetchTotalTools(){
+    async fetchTotalTools() {
       const total_pages = await this.fetchTools()
-      for(let i=1; i<=total_pages; i++){
-          const tempApiUrl = `${import.meta.env.VITE_API}?page=${i}`
-          try {
-            const res = await this.axios.get(tempApiUrl)
-            const apps = res.data.ai_works.data
-            this.applicationsBox.apps.push(...apps)
-          } catch (error) {
-            console.error(error)
-          }
+      for (let i = 1; i <= total_pages; i++) {
+        const tempApiUrl = `${import.meta.env.VITE_API}?page=${i}`
+        try {
+          const res = await this.axios.get(tempApiUrl)
+          const apps = res.data.ai_works.data
+          this.applicationsBox.apps.push(...apps)
+        } catch (error) {
+          console.error(error)
         }
-      this.tempPagination = this.pagination  
+      }
+      this.pagination.current_page = Number(this.pagination.current_page)
+      this.tempPagination = this.pagination
     },
-    async fetchTools(page=1){
+    async fetchTools(page = 1) {
       const apiUrl = `${import.meta.env.VITE_API}?page=${page}`
       try {
         const res = await this.axios.get(apiUrl)
         this.tempApplicationsBox.apps = res.data.ai_works.data
         this.pagination = res.data.ai_works.page
-        this.showTool(this.tempApplicationsBox)
         return res.data.ai_works.page.total_pages
       } catch (error) {
         console.error(error)
       }
     },
-    paginate(apps, perPage=6, curPage=1){
+    paginate(apps, perPage = 6, curPage = 1) {
       const totalPages = Math.ceil(apps.length / perPage)
 
-      if(curPage > totalPages){
+      if (curPage > totalPages) {
         curPage = totalPages
       }
 
       const minData = (curPage - 1) * perPage + 1
       const maxData = curPage * perPage
 
-
       const newAry = []
       apps.forEach((value, i) => {
         const num = i + 1
-        if(num>=minData && num<=maxData) {
+        if (num >= minData && num <= maxData) {
           newAry.push(value)
         }
-      });
+      })
 
       const page = {
         current_page: curPage,
@@ -303,16 +307,15 @@ export default {
       this.tempPagination = page
     },
 
-    changePage(page){
+    changePage(page) {
       this.tempPagination.current_page = page
       this.fetchTools(page)
     }
   },
   computed: {
-    searchTools(){
+    searchTools() {
       const query = this.searchWords
-      return this.tempApplicationsBox.apps.filter(app => app.title.includes(query))
-      
+      return this.tempApplicationsBox.apps.filter((app) => app.title.includes(query))
     }
   },
   created() {
@@ -335,5 +338,4 @@ export default {
 .split-line {
   margin: 0.75rem 0 0.25rem;
 }
-
 </style>
